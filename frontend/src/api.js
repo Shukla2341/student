807a641db8bf480be06f9b1164cdd3ec77244f2d
Shukla213 <aashutoshshukla985@gmail.com>
2026-04-23 02:46:41 +0530

    change api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://student-backend-5u8t.onrender.com/api",
 });
 
 export default API;
